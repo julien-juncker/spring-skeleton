cb1bc2212c21455c192744fc02aa6a34c3271f1b
julien-juncker <julien.juncker.pro@gmail.com>
2025-04-24 16:52:52 +0200

    feat(init): Init skeleton module

---
+++ apps/src/test/java/com/spring/skeleton/apps/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.skeleton.apps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
