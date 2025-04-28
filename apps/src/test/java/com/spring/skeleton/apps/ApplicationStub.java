1301d1f1b6cae1eae986e1b47520056a7e2540a9
julien-juncker <julien.juncker.pro@gmail.com>
2025-04-28 11:20:21 +0200

    refactor(skeleton): remove useless mockMVC

--- apps/src/test/java/com/spring/skeleton/apps/ApplicationStub.java
+++ apps/src/test/java/com/spring/skeleton/apps/ApplicationStub.java
@@ -1,15 +1,11 @@
 package com.spring.skeleton.apps;
 
 import com.spring.skeleton.Application;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, classes = {Application.class})
 @AutoConfigureMockMvc
 public class ApplicationStub {
 
-    @Autowired
-    public MockMvc mvc;
 }
